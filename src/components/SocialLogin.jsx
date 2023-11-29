f1615d51dd5a7cf79f99f83a7aa949e7ca9e1cfa
nafisaali0 <nafisaali20006@gmail.com>
2023-11-29 16:39:12 +0600

    setup google login

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,10 +1,40 @@
+import Swal from "sweetalert2";
+import useAuth from "../hooks/useAuth";
 
 const SocialLogin = () => {
+
+    const { googleLogIn } = useAuth()
+    const handleGoogle = () => {
+        googleLogIn()
+            .then(result => {
+                console.log(result.user);
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Signin successfully",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error)
+                return (
+                    Swal.fire({
+                        position: "top-end",
+                        icon: 'error',
+                        title: 'Try Again',
+                        text: 'Can Not signin With Google',
+                        footer: { error }
+                    })
+                )
+            })
+    }
+
     return (
         <div>
             <div>
                 <button
-                    // onClick={handleGoogle}
+                    onClick={handleGoogle}
                     className="w-full py-3 bg-gradient-to-r from-blue-500 to-indigo-800 text-white"
                     type="submit"
                     data-ripple-light="true"
--- src/shared/Header/NavBar.jsx
+++ src/shared/Header/NavBar.jsx
@@ -1,7 +1,9 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from '../../assets/images/icon/Logo-removebg.png'
 import './navbar.css'
+import useAuth from "../../hooks/useAuth";
 const NavBar = () => {
+    const { user } = useAuth();
 
     const navOptions = <>
         <nav className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""}>
@@ -10,90 +12,68 @@ const NavBar = () => {
             <NavLink to="/dashboard">Dashboard</NavLink>
             <NavLink to="/contact">Contact Us</NavLink>
         </nav>
-        {/* <li>
-            <Link to={'/dashboard/cart'}>
-                <button className="flex gap-1">
-                    <img className="w-7 h-7 filter invert" src={cartIcon} alt="" />
-                    <div className="badge">+ {cart.length}</div>
-                </button>
-            </Link>
-        </li> */}
-        {/* {
-            // user ? 'true': 'false'
-            // user ? condition ? 'double true' : 'one true' : 'false' 
-        } */}
-        {/* {
-            user && isAdmin && <li><Link to="/dashboard/adminHome">Dashboard</Link></li>
-        }
-        {
-            user && !isAdmin && <li><Link to="/dashboard/userHome">Dashboard</Link></li>
-        } */}
-
-
-        {/* {
-            user ?
-                <>
-                    <div className="dropdown dropdown-end text-[#474f85] font-bold font-roboto">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-20 lg:w-36 border-black border-2 rounded-full">
-                                <img src={user.photoURL ? user.photoURL : ``} />
-                            </div>
-                        </label>
-                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li><a>{user?.email}</a></li>
-                            <li><a>{user?.displayName}</a></li>
-                            <Link className="ml-3" to={'/signup'}><a>Add Another Account</a></Link>
-                            <li onClick={handleLogOut}><a>Logout</a></li>
-                        </ul>
-                    </div>
-                </>
-                :
-                <>
-                    <li><Link to={'/login'}>Login</Link></li>
-                </>
-        } */}
-
     </>
     return (
         <>
-            <div className=" drop-shadow-lg  bg-base-100">
-                <div className="navbar container mx-auto text-black">
-                    <div className="navbar-start ">
-                        <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost lg:hidden ">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                            </label>
+            {/* <div className=" drop-shadow-lg  bg-base-100"> */}
+            <div className="navbar container mx-auto text-black">
+                <div className="navbar-start ">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden ">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
 
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 bg-white">
-                                {navOptions}
-                            </ul>
-
-                        </div>
-                        <img className="w-16" src={logo} alt="" />
-                    </div>
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal px-1">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  rounded-box w-52 bg-white">
                             {navOptions}
                         </ul>
+
                     </div>
-                    <div className="navbar-end">
-                        <div className="dropdown dropdown-end text-[#474f85] font-bold font-roboto">
-                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                                <div className="w-20 lg:w-36 border-black border-2 rounded-full">
-                                    {/* <img src={user.photoURL ? user.photoURL : ``} /> */}
-                                    <img src="" alt="" />
+                    <img className="w-16" src={logo} alt="" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    {
+                        user ?
+                            <div className="dropdown dropdown-end text-[#474f85] font-bold font-roboto">
+                                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-20 lg:w-36 border-black border-2 rounded-full">
+                                        <img src={user.photoURL ? user.photoURL : ``} />
+                                    </div>
+                                </label>
+                                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                                    <li><a>{user.email}</a></li>
+                                    <li><a>{user.displayName}</a></li>
+                                    <Link className="ml-3" to={'/signup'}><a>Add Another Account</a></Link>
+                                    <li><a>Logout</a></li>
+                                    {/* onClick={handleLogOut} */}
+                                </ul>
+                            </div>
+                            :
+                            <>
+                                <div className="dropdown dropdown-end text-[#474f85] font-bold font-roboto">
+                                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                        <div className="w-20 lg:w-36 border-black border-2 rounded-full">
+                                            <img src={user?.photoURL ? user.photoURL : ``} />
+
+                                        </div>
+                                    </label>
+                                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                                        <li><a>{user?.email}</a></li>
+                                        <li><a>{user?.displayName}</a></li>
+                                        <Link className="ml-3" to={'/signup'}><a>Add Another Account</a></Link>
+                                        <li><a>Logout</a></li>
+                                        {/* onClick={handleLogOut} */}
+                                    </ul>
                                 </div>
-                            </label>
-                            {/* <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li><a>{user?.email}</a></li>
-                            <li><a>{user?.displayName}</a></li>
-                            <Link className="ml-3" to={'/signup'}><a>Add Another Account</a></Link>
-                            <li onClick={handleLogOut}><a>Logout</a></li>
-                        </ul> */}
-                        </div>
-                    </div>
+                            </>
+                    }
                 </div>
             </div>
+            {/* </div > */}
         </>
     );
 };
