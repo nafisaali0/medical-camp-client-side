c83783928fee422e207920df334d983640fe3dea
nafisaali0 <nafisaali20006@gmail.com>
2023-12-02 12:10:21 +0600

    load camp data using tanstack

--- src/hooks/useCamp.jsx
+++ src/hooks/useCamp.jsx
@@ -1,10 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxioslocalhost from "./useAxioslocalhost";
 
 const useCamp = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const axiosLocalhost = useAxioslocalhost()
+    const { data: camp = [], isPending: loading, refetch } = useQuery({
+        queryKey: ['camp'],
+        queryFn: async () => {
+            const res = await axiosLocalhost.get('/camp');
+            return res.data
+        }
+    })
+
+    return [camp, loading , refetch]
 };
 
 export default useCamp;
