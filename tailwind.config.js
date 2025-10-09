2b6d00a7acb1845cd8f97f8e265a17cecd11f013
nafisaali0 <nafisaali20006@gmail.com>
2025-10-09 23:33:15 +0600

    change tailwind.cofig.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      // borderColour: "#dbdbde",
+      // bodyColor: "#f3f3f3",
+      // primaryHover: "#302ba7",
+      // primaryColor: "#6a61d1",
+      // primaryLightColor: "#c6c2ec",
+      // black: "#000000",
+      // white: " #ffff",
+      // textSmallGray: "#6B6B6B",
+      // mainTheme: "#ffffffE6",
+      // iconRed: "#F40009",
+    },
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
-
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
+};
