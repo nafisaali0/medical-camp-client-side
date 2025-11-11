189bcf0c92d9229e2999e9dfd8ab2500aa0d36cb
nafisaali0 <nafisaali20006@gmail.com>
2025-11-12 03:28:47 +0600

    add bg

--- src/page/SignUp/SignUp.jsx
+++ src/page/SignUp/SignUp.jsx
@@ -5,9 +5,10 @@ import { Helmet } from "react-helmet-async";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
 import useAxioslocalhost from "../../hooks/useAxioslocalhost";
-import { BsArrowLeftSquareFill , BsPerson } from "react-icons/bs";
+import { BsArrowLeftSquareFill, BsPerson } from "react-icons/bs";
 import { TfiEmail } from "react-icons/tfi";
 import { TbLockPassword } from "react-icons/tb";
+import background from "../../assets/images/background/bg_form6.png"
 
 const SignUp = () => {
 
@@ -133,74 +134,84 @@ const SignUp = () => {
             </div> */}
 
             {/* new version */}
-            <div className="py-20 px-5 my-16">
-                <div className="max-w-xl mx-auto p-5 overflow-hidden flex flex-col bg-white border border-borderColour shadow-lg rounded-xl py-10 px-12 space-y-4">
-                    <div className="flex justify-between items-center mb-5 py-2">
+            <div
+                className="w-full h-screen"
+                style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                }}>
+                <div className="py-20 px-5">
+                    <div className="max-w-xl mx-auto p-5 overflow-hidden flex flex-col bg-white border border-borderColour shadow-lg rounded-xl py-10 px-12 space-y-4">
+                        <div className="flex justify-between items-center mb-5 py-2">
 
-                        <h5 className="text-textDark text-2xl font-medium">Join Our Health Campaign</h5>
+                            <h5 className="text-textDark text-2xl font-medium">Join Our Health Campaign</h5>
 
-                        <div title="Signin">
-                            <Link to={'/signin'}>
-                                <BsArrowLeftSquareFill className="text-2xl" />
-                            </Link>
+                            <div title="Signin">
+                                <Link to={'/signin'}>
+                                    <BsArrowLeftSquareFill className="text-2xl" />
+                                </Link>
+                            </div>
+
                         </div>
-                        
-                    </div>
-                    <div className="flex-auto">
+                        <div className="flex-auto">
 
-                        <form role="form" onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+                            <form role="form" onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
 
-                            <div className="relative flex items-center">
-                                <BsPerson className="absolute left-3 text-grayText text-lg" />
-                                <input
-                                    type="name" name="name" placeholder="Name"
-                                    {...register("name")}
-                                    className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
-                                />
-                            </div>
-                            <div className="relative flex items-center">
-                                <TfiEmail className="absolute left-3 text-grayText text-lg" />
-                                <input
-                                    type="email" name="email" placeholder="Email"
-                                    {...register("email", { required: true })}
-                                    className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
-                                />
-                            </div>
-                            <div className="flex flex-col">
                                 <div className="relative flex items-center">
-                                    <TbLockPassword className="absolute left-3 text-grayText text-lg" />
+                                    <BsPerson className="absolute left-3 text-grayText text-lg" />
                                     <input
-                                        type="password" name="password" placeholder="Password"
-                                        {...register("password", {
-                                            required: true,
-                                            minLength: 6,
-                                            maxLength: 20,
-                                            pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
-                                        })}
+                                        type="name" name="name" placeholder="Name"
+                                        {...register("name")}
                                         className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
                                     />
                                 </div>
-                                <div className="text-grayText text-sm mt-2">
-                                    {errors.password?.type === 'required' && <p>Password is required</p>}
-                                    {errors.password?.type === 'minLength' && <p>Password must be 6 characters</p>}
-                                    {errors.password?.type === 'maxLength' && <p>Password must be less than 20 characters</p>}
+                                <div className="relative flex items-center">
+                                    <TfiEmail className="absolute left-3 text-grayText text-lg" />
+                                    <input
+                                        type="email" name="email" placeholder="Email"
+                                        {...register("email", { required: true })}
+                                        className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
+                                    />
                                 </div>
-                            </div>
+                                <div className="flex flex-col">
+                                    <div className="relative flex items-center">
+                                        <TbLockPassword className="absolute left-3 text-grayText text-lg" />
+                                        <input
+                                            type="password" name="password" placeholder="Password"
+                                            {...register("password", {
+                                                required: true,
+                                                minLength: 6,
+                                                maxLength: 20,
+                                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                            })}
+                                            className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
+                                        />
+                                    </div>
+                                    <div className="text-grayText text-sm mt-2">
+                                        {errors.password?.type === 'required' && <p>Password is required</p>}
+                                        {errors.password?.type === 'minLength' && <p>Password must be 6 characters</p>}
+                                        {errors.password?.type === 'maxLength' && <p>Password must be less than 20 characters</p>}
+                                    </div>
+                                </div>
 
-                            <div className=''>
-                                <button type="submit" className='w-full border px-4 py-2 rounded-lg text-md font-medium text-white border-btnColor bg-btnColor'>Sign up</button>
-                            </div>
+                                <div className=''>
+                                    <button type="submit" className='w-full border px-4 py-2 rounded-lg text-md font-medium text-white border-btnColor bg-btnColor'>Sign up</button>
+                                </div>
 
-                            <div className="text-center">
-                                <p className="font-medium text-sm">or</p>
-                            </div>
-                            <div className="flex-auto">
-                                <SocialLogin />
-                            </div>
+                                <div className="text-center">
+                                    <p className="font-medium text-sm">or</p>
+                                </div>
+                                <div className="flex-auto">
+                                    <SocialLogin />
+                                </div>
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </>
     );
--- src/page/Signin/Signin.jsx
+++ src/page/Signin/Signin.jsx
@@ -6,6 +6,7 @@ import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
 import { TfiEmail } from "react-icons/tfi";
 import { TbLockPassword } from "react-icons/tb";
+import background from "../../assets/images/background/bg_form6.png"
 
 const Signin = () => {
 
@@ -87,72 +88,81 @@ const Signin = () => {
             </div> */}
 
             {/* new version */}
-            <div className="py-20 px-5 my-16">
-                <div className="max-w-xl mx-auto p-5 overflow-hidden flex flex-col bg-white border border-borderColour shadow-lg rounded-xl py-10 px-12 space-y-4">
-                    <div className="mb-5">
-                        <h5 className="text-textDark text-2xl font-medium">Welcome Back</h5>
-                    </div>
-                    <div className="flex-auto">
+            <div
+                className="w-full h-screen"
+                style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                }}>
+                <div className="py-20 px-5">
+                    <div className="max-w-xl mx-auto p-5 overflow-hidden flex flex-col bg-white border border-borderColour shadow-lg rounded-xl py-10 px-12 space-y-4">
+                        <div className="mb-5">
+                            <h5 className="text-textDark text-2xl font-medium">Welcome Back</h5>
+                        </div>
+                        <div className="flex-auto">
 
-                        <form role="form" onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+                            <form role="form" onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
 
-                            <div className="flex flex-col">
+                                <div className="flex flex-col">
 
-                                <div className="relative flex items-center">
-                                    <TfiEmail className="absolute left-3 text-grayText text-lg" />
-                                    <input
-                                        type="email" name="email" placeholder="Email"
-                                        {...register("email", { required: true })}
-                                        className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
-                                    />
+                                    <div className="relative flex items-center">
+                                        <TfiEmail className="absolute left-3 text-grayText text-lg" />
+                                        <input
+                                            type="email" name="email" placeholder="Email"
+                                            {...register("email", { required: true })}
+                                            className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
+                                        />
+                                    </div>
+
+                                    <div className="text-grayText text-sm mt-2">
+                                        {errors?.email?.type === 'required' && <p>Email is required</p>}
+                                    </div>
+
                                 </div>
 
-                                <div className="text-grayText text-sm mt-2">
-                                    {errors?.email?.type === 'required' && <p>Email is required</p>}
+                                <div className="flex flex-col">
+                                    <div className="relative flex items-center">
+                                        <TbLockPassword className="absolute left-3 text-grayText text-lg" />
+                                        <input
+                                            type="password" name="password" placeholder="Password"
+                                            {...register("password", {
+                                                required: true,
+                                                minLength: 6,
+                                                maxLength: 20,
+                                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                            })}
+                                            className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
+                                        />
+                                    </div>
+                                    <div className="text-grayText text-sm mt-2">
+                                        {errors?.password?.type === 'required' && <p>Password is required</p>}
+                                    </div>
                                 </div>
 
-                            </div>
-
-                            <div className="flex flex-col">
-                                <div className="relative flex items-center">
-                                    <TbLockPassword className="absolute left-3 text-grayText text-lg" />
-                                    <input
-                                        type="password" name="password" placeholder="Password"
-                                        {...register("password", {
-                                            required: true,
-                                            minLength: 6,
-                                            maxLength: 20,
-                                            pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
-                                        })}
-                                        className="text-sm ease-soft block w-full rounded-lg border border-solid border-borderColour py-2 pl-10 pr-3 font-medium text-textSmallGray transition-all focus:border-borderColour focus:bg-mainTheme focus:text-grayText focus:outline-none"
-                                    />
+                                <div>
+                                    <button type="submit" className='w-full border px-4 py-2 rounded-lg text-md font-medium text-white border-btnColor bg-btnColor'>Sign In</button>
                                 </div>
-                                <div className="text-grayText text-sm mt-2">
-                                    {errors?.password?.type === 'required' && <p>Password is required</p>}
+
+                                <div className="flex items-center justify-center gap-2 text-md font-medium">
+                                    <p>New to Amelia?</p>
+                                    <Link
+                                        to={'/signup'}
+                                        title="Signup">
+                                        <p className="text-primaryDark">Create New Account</p>
+                                    </Link>
                                 </div>
-                            </div>
 
-                            <div>
-                                <button type="submit" className='w-full border px-4 py-2 rounded-lg text-md font-medium text-white border-btnColor bg-btnColor'>Sign In</button>
-                            </div>
+                                <div className="text-center">
+                                    <p className="font-medium text-sm">or</p>
+                                </div>
 
-                            <div className="flex items-center justify-center gap-2 text-md font-medium">
-                                <p>New to Amelia?</p>
-                                <Link
-                                    to={'/signup'}
-                                    title="Signup">
-                                    <p className="text-primaryDark">Create New Account</p>
-                                </Link>
-                            </div>
-
-                            <div className="text-center">
-                                <p className="font-medium text-sm">or</p>
-                            </div>
-
-                            <div className="flex-auto">
-                                <SocialLogin />
-                            </div>
-                        </form>
+                                <div className="flex-auto">
+                                    <SocialLogin />
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
