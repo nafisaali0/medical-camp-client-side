ca1d2cfcc7738cc1a3fd71a8f589c230cd0c1087
nafisaali0 <nafisaali20006@gmail.com>
2025-11-15 02:17:10 +0600

    complete logic of signin and signup

--- src/page/SignUp/SignUp.jsx
+++ src/page/SignUp/SignUp.jsx
@@ -88,74 +88,7 @@ const SignUp = () => {
             <Helmet>
                 <title>Amelia | Signup</title>
             </Helmet>
-
-            {/* old version */}
-            {/* <div className="overflow-hidden" >
-                <div className='container mx-auto flex flex-col justify-center items-center drop-shadow-lg md:pt-12 lg:pt-20'>
-                    <div className="bg-base-100 m-10 p-7 md:w-[700px]">
-                        <div className="text-black my-5 font-bold text-2xl">
-                            <h2>Please create your account</h2>
-                        </div>
-                        <div>
-                            <div className="my-10">
-                                <SocialLogin></SocialLogin>
-                            </div>
-                            <div className="my-10 text-center text-xl font-bold">
-                                <p>OR</p>
-                            </div>
-                        </div>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <div>
-                                <input type="name" {...register("name")} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Name" />
-                            </div>
-                            <div>
-                                <input type="url" {...register("photo")} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Image" />
-                            </div>
-                            <select {...register("role")} className="select select-bordered w-full my-5">
-                                <option disabled selected>Select Your Role</option>
-                                <option>Admin</option>
-                                <option>Healthcare Professionals</option>
-                                <option>Participant</option>
-                            </select>
-                            <div>
-                                <input type="number" {...register("phone")} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Phone Number" />
-                            </div>
-                            <div className="relative z-0 w-full mb-6 group">
-                                <input type="email" {...register("email", { required: true })} name="email" className="block py-2 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300   focus:outline-none focus:border-blue-600 peer" placeholder="Email" />
-                                {errors.email && <span>This field is required</span>}
-                            </div>
-                            <div className="relative z-0 w-full mb-7 group">
-                                <input type="password"  {...register("password", {
-                                    required: true,
-                                    minLength: 6,
-                                    maxLength: 20,
-                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
-                                })}
-                                placeholder="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" />
-                                {errors.password?.type === 'required' && <p>Password is required</p>}
-                                {errors.password?.type === 'minLength' && <p>Password must be 6 characters</p>}
-                                {errors.password?.type === 'maxLength' && <p>Password must be less than 20 characters</p>}
-                                {errors.password?.type === 'pattern' && <p>Password must have one Uppercase one lower case, one number and one special character.</p>}
-                            </div>
-                            <div>
-                                <textarea type="number" {...register("address")} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Address" />
-                            </div>
-                            <div className="flex gap-2 md:gap-5 lg:gap-10 items-center my-10">
-                                <div className="relative z-0 group">
-                                    <button type="submit" className="text-white md:w-full bg-gradient-to-r from-blue-500 to-indigo-800  font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-cent">SIGN UP</button>
-                                </div>
-                                <div className="relative z-0 group">
-                                    <Link to={'/signin'}>
-                                        <button className="text-white md:w-full  bg-gradient-to-r from-blue-500 to-indigo-800 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center">SIGN IN</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> */}
-
-            {/* new version */}
+            
             <div
                 className="w-full h-screen"
                 style={{
--- src/page/Signin/Signin.jsx
+++ src/page/Signin/Signin.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../../components/SocialLogin";
 import { useForm } from "react-hook-form"
 import { Helmet } from "react-helmet-async";
@@ -10,22 +10,22 @@ import background from "../../assets/images/background/bg_form6.png"
 
 const Signin = () => {
 
-    const { register, handleSubmit, formState: { errors }, } = useForm();//from react hook 
+    const { register, handleSubmit, formState: { errors }, } = useForm(); 
     const { signInUser } = useAuth();
-    const location = useLocation();
     const navigate = useNavigate();
 
-    // after sign in user back desire path or page
-    const from = location.state?.from?.pathname || '/';
 
     const onSubmit = (data) => {
         console.log(data)
-
+        
         // signin authentication
         signInUser(data.email, data.password)
+
             .then(result => {
+
                 const user = result.user;
                 console.log(user);
+
                 Swal.fire({
                     position: "top-end",
                     icon: "success",
@@ -37,7 +37,9 @@ const Signin = () => {
                         popup: 'animate__animated animate__fadeOutUp'
                     }
                 });
-                navigate(from, { replace: true });
+
+                navigate('/')
+
             })
 
     }
@@ -48,46 +50,6 @@ const Signin = () => {
                 <title>Amelia | Signin</title>
             </Helmet>
 
-            {/* old version */}
-            {/* <div className="w-[100%] lg:h-[100vh] md:h-[100%]" >
-                <div className='container mx-auto overflow-hidden flex flex-col justify-center items-center drop-shadow-lg md:pt-12 lg:pt-20'>
-                    <div className="bg-base-100 m-10 p-7 md:w-[700px]">
-                        <div className="text-black my-5 font-bold text-2xl">
-                            <h2>Welcome back please signin <br /> to your account</h2>
-                        </div>
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <div className="my-10">
-                                <SocialLogin></SocialLogin>
-                            </div>
-                            <div className="my-10 text-center text-xl font-bold">
-                                <p>OR</p>
-                            </div>
-                            <div className="relative z-0 w-full mb-6 group">
-                                <input type="email" {...register("email")} name="email" className="block py-2 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300   focus:outline-none focus:border-blue-600 peer" placeholder=" " required />
-                                {errors?.email && <span>This field is required</span>}
-                                <label className=" absolute text-sm text-gray-500 pb-5 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0]   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-blue-600">User Email</label>
-                            </div>
-                            <div className="relative z-0 w-full mb-7 group">
-                                <input type="password" {...register("password")} name="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                                {errors?.password?.type === 'required' && <p>Password is required</p>}
-                                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-                            </div>
-                            <div className="flex gap-2 md:gap-5 lg:gap-10 items-center my-10">
-                                <div className="relative z-0 group">
-                                    <button type="submit" className="text-white md:w-full  bg-gradient-to-r from-blue-500 to-indigo-800  font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-cent">SIGN IN</button>
-                                </div>
-                                <div className="relative z-0 group">
-                                    <Link to={'/signup'}>
-                                        <button className="text-white md:w-full  bg-gradient-to-r from-blue-500 to-indigo-800  font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center">SIGN UP</button>
-                                    </Link>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> */}
-
-            {/* new version */}
             <div
                 className="w-full h-screen"
                 style={{
