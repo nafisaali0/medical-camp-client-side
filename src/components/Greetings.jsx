cc99909e1151f0628c09de2ea19f765c7408ad17
nafisaali0 <nafisaali20006@gmail.com>
2026-01-08 23:10:34 +0600

    change

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -10,16 +10,16 @@ const Greetings = () => {
 
         if (currentHour >= 5 && currentHour < 12) {
             setGreetingsStatus("Good Morning")
-            console.log("Good Morning");
+            // console.log("Good Morning");
         } else if (currentHour >= 12 && currentHour < 17) {
             setGreetingsStatus("Good Afternoon")
-            console.log("Good Afternoon")
+            // console.log("Good Afternoon")
         } else if (currentHour >= 17 && currentHour < 19) {
-            setGreetingsStatus("Good Evening")
+            // setGreetingsStatus("Good Evening")
             console.log("Good Evening")
         } else if (currentHour >= 19 || currentHour < 5) {
             setGreetingsStatus("Good Night")
-            console.log("Good Night")
+            // console.log("Good Night")
         } else {
             console.log("not work")
         }
